66154cf903554b4c2f8884c7858cd739ddffeb5f
Yash Mishra <yashrkmfedu6927@gmail.com>
2023-04-26 01:24:48 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,46 @@
+# Git-Inspector
+
+<img src="./assets/images/Screenshot (261).png">
+<img src="./assets/images/Screenshot (263).png">
+<img src="./assets/images/Screenshot (264).png">
+
+Git-Inspector is a web application that allows you to search for GitHub users and view their basic information such as user profile, user ID, name, bio, followers, following, public repositories, Twitter account name, current working company, GitHub joining date, and more. The application also includes a dark mode feature for better user experience.
+
+Note: By default, Git-Inspector displays information for the GitHub user with the username "yashmishra0101" (which is myself).
+
+## Features
+
+- Search for GitHub users using their username
+- View basic user information including user ID, user profile, name, bio, followers, following, public repositories, Twitter account name, current working company, and GitHub joining date
+- Toggle between light and dark mode for better user experience
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Usage
+
+1. Enter a GitHub username in the search bar and click the search button.
+2. The user information will be displayed below the search bar.
+3. To toggle between light and dark mode, click the moon/sun icon in the top right corner.
+
+# Getting Started
+
+To get started with this weather app, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/YashMishra0101/GitInspector`
+2. Navigate to the project directory: `cd GitInspector`
+3. Open `index.html` in your web browser
+
+ 
+## Contributing
+
+If you would like to contribute to this GitInspector, please open an issue or submit a pull request. All contributions are welcome!
+
+
+## Feedback
+
+If you have any questions or suggestions, please feel free to contact me on LinkedIn 
+https://www.linkedin.com/in/yash-mishra-356280223
---
+++ script.js
@@ -0,0 +1,107 @@
+const moonsun_image=document.querySelector('[data-moon-sun-image]');
+const main_container =document.querySelector('[data-main-container]');
+const search_button=document.querySelector('[data-searchButton]')
+const input_field=document.querySelector('[data-input-field]')
+const user_info =document.querySelector('[data-user-info-container]');
+const user_image =document.querySelector('[data-userImage]');
+const user_Name =document.querySelector('[data-userName]');
+const user_Id =document.querySelector('[data-userId]');
+const user_desc =document.querySelector('[data-userDesc]');
+const user_joining_date =document.querySelector('[data-userJoiningDate]');
+const use_repos =document.querySelector('[data-useRepos]');
+const user_followers =document.querySelector('[data-userFollowers]');
+const user_following =document.querySelector('[data-userFollowing]');
+const user_loc =document.querySelector('[data-userLoc]');
+const user_bioLink =document.querySelector('[data-userBioLink]');
+const user_twitter =document.querySelector('[data-userTwitter]');
+const user_Work =document.querySelector('[data-userWork]');
+const dark_light =document.querySelector('[dark-light]');
+const error_page =document.querySelector('[data-error-page]');
+const error_page_two =document.querySelector('[data-error-page-two]');
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const root=document.documentElement.style;
+
+const darkMode=()=>{
+  root.setProperty('--bg-color','#2b3442');
+  moonsun_image.src="./assets/images/sun-icon.svg"
+}
+
+const lightMode=()=>{
+  root.setProperty('--bg-color', 'white')
+  moonsun_image.src="./assets/images/moon-icon.svg"
+}
+
+moonsun_image.addEventListener('click',()=>{
+  if(root.getPropertyValue('--bg-color')!=='white'){
+    lightMode();
+  }
+  else{
+    darkMode();
+  }
+})
+
+async function fetchData(user) {
+  try {
+    user_info.style.display='block'
+    error_page.classList.remove('active')
+    error_page_two.classList.remove('active')
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    const data = await response.json();
+    console.log(data)
+    if(data.message==="Not Found"){
+    input_field.value="Enter a valid name 😕";
+    user_info.style.display='none'
+    error_page_two.classList.add('active')
+
+    }
+    user_Name.innerHTML=`${data.name}`;
+    user_Id.innerHTML=`@${data.login}`; 
+    user_Id.href=`https://github.com/${data.login}`;
+    user_desc.innerHTML=data.bio===null ? 'This profile has no bio' : `${data.bio}`;
+    user_image.src=`${data.avatar_url}`;
+    user_joining_date.innerHTML = `${data.created_at}`;
+    const createdAt = new Date(data.created_at);
+    const year = createdAt.getFullYear();
+    const month = months[createdAt.getMonth()];
+    const date = String(createdAt.getDate()).padStart(2, '0');
+    user_joining_date.innerHTML = `${year}-${month}-${date}`;
+    use_repos.innerHTML=`${data.public_repos}`;
+    user_followers.innerHTML=`${data.followers}`;
+    user_following.innerHTML=`${data.following}`;
+    user_loc.innerHTML= data.location === null ? 'Not available' :`${data.location}`;
+    user_bioLink.href=data.html_url === null ? 'Not available' : `${data.html_url}`;
+    user_bioLink.innerText=data.html_url === null ? 'Not available' : `${data.html_url}`;
+    user_twitter.innerHTML=data.twitter_username=== null ? 'Not available' :`${data.twitter_username}`;
+    user_Work.innerHTML=data.company === null ? 'Not available' :`${data.company}`;
+  } 
+  catch {
+    input_field.value="Api is not working 😵‍💫🚑🥴";
+    user_info.style.display='none'
+    error_page.classList.add('active')
+    error_page_two.classList.remove('active')
+  }
+}
+
+const hkirat = "yashmishra0101";
+if(input_field.value===""){
+  fetchData(hkirat);
+}
+ 
+search_button.addEventListener('click', () =>{
+  if(input_field.value ===''){
+    input_field.value="Enter a valid name"
+    user_info.style.display='none';
+    error_page.classList.add('active')
+  }
+  else{
+  const searchValue =input_field.value;
+  fetchData(searchValue);  
+  search_button.style.pointerEvents ='auto';
+  }
+});
+
+
+
+
+
+
